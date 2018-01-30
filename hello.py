63a69c76477cc9a77717d9c47d221676f24ca65a
Hannah Rosenbaum <hanrosen@umich.edu>
2018-01-30 09:50:51 -0500

    added about me

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
 print('My name is Hannah')
 print("I was born in July")
+print('My favorite food is Chocolate')
+print('My favorite type of music is pop')
+print('im a cancer')
+print('rosner')
+
 print('My name is Danielle')
 print('I was born in June')
